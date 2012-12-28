f5068a4de0ae586e8eddf4aa5792ca6ad72d31f8
bdetweiler <bdetweiler@gmail.com>
2012-12-28 18:38:50 +0000

    Everything here seems right, but it isn't giving me the right answer apparently.

--- src/net/projecteuler/problem44/Problem44.java
+++ src/net/projecteuler/problem44/Problem44.java
@@ -8,7 +8,35 @@ public class Problem44 {
     
     @Test
     public void testPentagonal() { 
-        System.out.println(EulerUtils.pentagonalNumber(4));
-        System.out.println(EulerUtils.pentagonalNumber(7));
+       
+        int limit = 10000;
+        long min = 50000000;
+        int minI = 50000000;
+        int minJ = 50000000;
+        long minPentI = 50000000;
+        long minPentJ = 50000000;
+    
+        for(int i = 1; i < limit; ++i) {
+            for(int j = i + 1; j < limit && (j - i) < 5000; ++j) {
+                long pentI = EulerUtils.pentagonalNumber(i).longValue();
+                long pentJ = EulerUtils.pentagonalNumber(j).longValue();
+                if(EulerUtils.isPentagonal(pentI + pentJ) && EulerUtils.isPentagonal(Math.abs(pentI - pentJ))) {
+                    System.out.println(pentI + ", " + pentJ + ", " + (pentI + pentJ) + ", " + (pentJ - pentI));
+                    if(Math.abs(pentI - pentJ) < min) {
+                        min = pentJ - pentI;
+                        minI = i;
+                        minJ = j;
+                        minPentI = pentI;
+                        minPentJ = pentJ;
+                    }
+                }
+            }
+        }
+        
+        System.out.println("min = " + min);
+        System.out.println("minPentI = " + minPentI);
+        System.out.println("minPentJ = " + minPentJ);
+        System.out.println("minI = " + minI);
+        System.out.println("minJ = " + minJ);
     }
 }
