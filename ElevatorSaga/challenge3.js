e8e2059b8ae28c53ac387d1aa5fcd206a846b5b6
bdetweiler <bdetweiler@37941459-6aff-06ef-fe37-bbcdd6a559c6>
2015-02-20 05:54:51 +0000

    Adding Elevator Saga

---
+++ ElevatorSaga/challenge3.js
@@ -0,0 +1,16 @@
+{
+    init: function(elevators, floors) {
+        var elevator = elevators[0]; // Let's use the first elevator
+        elevator.on("idle", function() {
+            // The elevator is idle, so let's go to all the floors (or did we forget one?)
+            elevator.goToFloor(0);
+            elevator.goToFloor(1);
+            elevator.goToFloor(2);
+            elevator.goToFloor(3);
+            elevator.goToFloor(4);
+        });
+    },
+    update: function(dt, elevators, floors) {
+        // We normally don't need to do anything here
+    }
+}
